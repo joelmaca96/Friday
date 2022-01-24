9e4da3360f7952c608adf75c8d56d25992481206
joelmaca96 <joelmaca96@gmail.com>
2022-01-24 09:16:23 +0100

    Orden 2

--- constants.py
+++ constants.py
@@ -1,15 +1,26 @@
+from enum import Enum
 
 #Constantes para Friday
 NAME = 'friday'
 TEMPORIZADOR = ['temporizador', 'cuenta', 'cronometra', 'alarma', 'avisame']
 HORA = ['hora']
 FECHA = ['fecha', 'dia']
+OPERACIONES = ['mas', 'menos', 'entre', 'dividido por', 'multiplicado por', 'por']
 
-TEMP_CONSTANTS = [TEMPORIZADOR, HORA, FECHA]
+TEMP_QUESTIONS = [HORA, FECHA]
 VOL_UP = ['sube', 'subir', 'eleva', 'aumenta']
 VOL_DOWN = ['baja', 'bajar', 'disminuye', 'reduce']
 SALUDOS = ['hola', 'buenos dias', 'buenas tardes', 'kaixo', 'egun on', 'buenas noches', 'ole']
 DESPEDIDAS = ['adios', 'hasta luego', 'que tengas un buen dia', 'calla', 'agur', 'aio', 'hasta pronto', 'callate', 'que te den']
+
+#Categorias de texto
+class Categories(Enum):
+    EXIT      = 1
+    AJUSTES   = 2
+    TEMPO     = 3
+    SALUDO    = 4
+    DESPEDIDA = 5
+
 
 #Substituciones para el preprocesado del texto
 tildes = (
